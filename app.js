8ef5ef471fc8e5fd16b5e64e83fe797eaaf9198f
patrik.aradi <patrik.aradi@supercharge.io>
2020-05-23 17:28:26 +0200

    connect to db

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+console.log('In app.js!')
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -38,10 +37,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const mongoConnect = require('./util/database').mongoConnect;
-
-mongoConnect(() => {
-  app.listen(3000);
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mongo = require(".././util/database.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/generate-link', function (req, res, next) {
-  res.render('link', {link: "faszom"})
+  let count = mongo.getDb().collection("links").count()
+  let links = mongo.getDb().collection("links").find()
+  console.log(`Num of links ${count}`)
+  console.log(links)
+  res.render('link', {link: links})
 });
 
 module.exports = router;
